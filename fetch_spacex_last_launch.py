b29cad77094a07885aa2cd6de1366b10eaf48b86
vlada-97 <108741329+vlada-97@users.noreply.github.com>
2022-11-15 21:22:39 +0300

    Update fetch_spacex_last_launch.py

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -4,18 +4,24 @@ import requests
 import pathlib
 from dotenv import load_dotenv
 
+import download_image
 
-def get_image(url, way, params = None):
-    response = requests.get(url, params = params)
+
+def fetch_spacex_your_launch(folder_name, id):
+
+    url_spacex = f"https://api.spacexdata.com/v5/launches/{id}"
+    response = requests.get(url_spacex)
     response.raise_for_status()
+    if response.ok:
+        photos_spacex = [url_spacex for url_spacex in response.json()['links']['flickr']['original']]
+          
+    for number, link in enumerate(photos_spacex):
+        file_path = f"{folder_name}/spacex{number}.jpg"
+        download_image.get_image(link, file_path)
 
-    with open(way, 'wb') as file:
-        file.write(response.content)
 
-
-def fetch_spacex_last_launch(folder_name, id):
-
-    url_spacex = "https://api.spacexdata.com/v3/launches/{}".format(id)
+def fetch_spacex_last_launch(folder_name):
+    url_spacex = "https://api.spacexdata.com/v3/launches/"
     response = requests.get(url_spacex)
     response.raise_for_status()
 
@@ -27,26 +33,21 @@ def fetch_spacex_last_launch(folder_name, id):
           
     for number, link in enumerate(photos_spacex):
         file_path = f"{folder_name}/spacex{number}.jpg"
-        get_image(link, file_path)
+        download_image.get_image(link, file_path)
 
-
-def main():
-    load_dotenv()
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url")
-    args = parser.parse_args()
-    url = args.url
-
-    folder_spacex = os.environ["FOLDER_SPACEX"]
-    pathlib.Path(folder_spacex).mkdir(parents=True, exist_ok=True)
-
-
-    try:
-        fetch_spacex_last_launch(folder_spacex, str(url))
-    except requests.exceptions.HTTPError as error:
-        url = "5eb87d42ffd86e000604b384"
-        fetch_spacex_last_launch(folder_spacex, str(url))
-        print("Can't get data from server:\n{0}".format(error))
 
 if __name__ == "__main__":
-    main()
+    load_dotenv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("id")
+    args = parser.parse_args()
+    id = args.id
+
+    folder_spacex = os.getenv("FOLDER_SPACEX")
+    pathlib.Path(folder_spacex).mkdir(parents=True, exist_ok=True)
+
+    try:
+        fetch_spacex_your_launch(folder_spacex, str(id))
+    except requests.exceptions.HTTPError as error:
+        print("Can't get data from server:\n{0}.\nDownloading the last launch.".format(error))
+        fetch_spacex_last_launch(folder_spacex)
