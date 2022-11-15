0bf2fe7ce17e6d2534dbc8ea529ae54d34554fb7
vlada-97 <108741329+vlada-97@users.noreply.github.com>
2022-11-15 21:21:56 +0300

    Update fetch_nasa_epic.py

--- fetch_nasa_epic.py
+++ fetch_nasa_epic.py
@@ -2,14 +2,8 @@ import os
 import pathlib
 import requests
 from datetime import datetime 
-
-
-def get_image(url, way, params = None):
-    response = requests.get(url, params = params)
-    response.raise_for_status()
-
-    with open(way, 'wb') as file:
-        file.write(response.content)
+from dotenv import load_dotenv
+import download_image
 
 
 def fetch_nasa_epic(folder_name, api_key):
@@ -29,10 +23,12 @@ def fetch_nasa_epic(folder_name, api_key):
         epic_image_date = datetime.fromisoformat(epic_image_date).strftime("%Y/%m/%d")
         link_path = f"https://api.nasa.gov/EPIC/archive/natural/{epic_image_date}/png/{filename}.png"
         file_path = f"{folder_name}/{filename}.png"
-        get_image(link_path, file_path, params)
+        download_image.get_image(link_path, file_path, params)
 
-def main():
-    api_key = os.environ['API_KEY']
-    folder_epic = os.environ["FOLDER_EPIC"]
+
+if __name__ == '__main__':
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
+    folder_epic = os.getenv("FOLDER_EPIC")
     pathlib.Path(folder_epic).mkdir(parents=True, exist_ok=True)
     fetch_nasa_epic(folder_epic, api_key)
