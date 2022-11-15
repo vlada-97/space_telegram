ff60f271f8c493bdea2369cb400e64bbaa23b57a
vlada-97 <108741329+vlada-97@users.noreply.github.com>
2022-11-15 21:24:08 +0300

    Update tg_bot_random_image.py

--- tg_bot_random_image.py
+++ tg_bot_random_image.py
@@ -1,14 +1,29 @@
+import argparse
 import os
 import random
-import argparse
-
-import telegram
-
-from dotenv import load_dotenv
+import requests
 from os import listdir
-
 from os.path import isfile
 from os.path import join as joinpath
+
+import telegram
+from dotenv import load_dotenv
+
+def random_photo(bot, chat_id):
+    Path = 'nasa' + '/'
+    for image_file in listdir(Path):
+        if isfile(joinpath(Path, image_file)):
+            filesindir = os.listdir(Path)
+            for files in filesindir:
+                random.shuffle(filesindir)
+                path_file = os.path.join(files)
+        bot.send_document(chat_id=chat_id, document=open(Path + path_file, 'rb'))
+
+
+def send_photo(bot, chat_id, img):
+    bot.send_document(chat_id=chat_id, document=open(img, 'rb'))
+    response = requests.get(img)
+    response.raise_for_status()
 
 
 if __name__ == '__main__':
@@ -19,20 +34,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     img = args.img
     
-    chat_id = os.environ['CHAT_ID']
-    bot_token = os.environ['BOT_TOKEN']
+    bot_token = os.getenv('BOT_TOKEN')
+    chat_id = os.getenv('CHAT_ID')
     bot = telegram.Bot(token=bot_token)
 
-
     try:
-        bot.send_document(chat_id=chat_id, document=open(img, 'rb'))
+        send_photo(bot, chat_id, img)
     except:
-        print("No such file or directory")
-        Path = 'test/'
-        for image_file in listdir(Path):
-            if isfile(joinpath(Path, image_file)):
-                filesindir = os.listdir(Path)
-                for files in filesindir:
-                    random.shuffle(filesindir)
-                    path_file = os.path.join(files)
-        bot.send_document(chat_id=chat_id, document=open(Path + path_file, 'rb'))
+        random_photo(bot, chat_id)
