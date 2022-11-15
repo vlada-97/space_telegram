34ec8852cf54e451c08f6c388305b1e78892fd0b
vlada-97 <108741329+vlada-97@users.noreply.github.com>
2022-11-15 21:23:12 +0300

    Update tg_bot_all_images.py

--- tg_bot_all_images.py
+++ tg_bot_all_images.py
@@ -1,18 +1,19 @@
-import os
 import argparse
+import os
 import random
 import time
-
 from os import listdir
 from os.path import isfile
 from os.path import join as joinpath
+from tkinter import BOTH
 
+import requests
 import schedule
 import telegram
 from dotenv import load_dotenv
 
 
-def upload_images_to_tg(Path, bot_obj):
+def upload_images_to_tg(Path, bot, chat_id):
     Path = Path + '/'
     for image_file in listdir(Path):
         if isfile(joinpath(Path, image_file)):
@@ -29,18 +30,21 @@ def shedule():
 
 if __name__ == '__main__':
     load_dotenv()
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("path")
-    parser.parse_args()
+    parser.add_argument("Path")
     args = parser.parse_args()
-    Path = args.path
-    
-    bot_token = os.environ['BOT_TOKEN']
+    Path = args.Path
+    Path = Path + '/'
+
+    bot_token = os.getenv('BOT_TOKEN')
+    chat_id = os.getenv('CHAT_ID')
+    hours_break = os.getenv('HOURS_BREAK')
+
     bot = telegram.Bot(token=bot_token)
-    chat_id = os.environ['CHAT_ID']
-    hours_break = os.environ['HOURS_BREAK']
+    bot.token = bot._validate_token(bot_token)
 
     while True:
-        upload_images_to_tg(Path, bot)
+        upload_images_to_tg(Path, bot, chat_id)
         schedule.run_pending()
-        time.sleep(int(hours_break)*60)
+        time.sleep(int(hours_break)*60*60)
