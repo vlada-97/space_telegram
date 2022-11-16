2f323694f020a8d8e0aa921facb623b3ea4c43bc
vlada-97 <108741329+vlada-97@users.noreply.github.com>
2022-11-16 13:46:08 +0300

    Update fetch_spacex_last_launch.py

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -7,9 +7,9 @@ from dotenv import load_dotenv
 import download_image
 
 
-def fetch_spacex_your_launch(folder_name, id):
+def fetch_spacex_your_launch(folder_name, id_spacex):
 
-    url_spacex = f"https://api.spacexdata.com/v5/launches/{id}"
+    url_spacex = f"https://api.spacexdata.com/v5/launches/{id_spacex}"
     response = requests.get(url_spacex)
     response.raise_for_status()
     if response.ok:
@@ -21,7 +21,7 @@ def fetch_spacex_your_launch(folder_name, id):
 
 
 def fetch_spacex_last_launch(folder_name):
-    url_spacex = "https://api.spacexdata.com/v3/launches/"
+    url_spacex = "https://api.spacexdata.com/v5/launches/"
     response = requests.get(url_spacex)
     response.raise_for_status()
 
@@ -39,15 +39,15 @@ def fetch_spacex_last_launch(folder_name):
 if __name__ == "__main__":
     load_dotenv()
     parser = argparse.ArgumentParser()
-    parser.add_argument("id")
+    parser.add_argument("id_spacex")
     args = parser.parse_args()
-    id = args.id
+    id_spacex = args.id_spacex
 
     folder_spacex = os.getenv("FOLDER_SPACEX")
     pathlib.Path(folder_spacex).mkdir(parents=True, exist_ok=True)
 
     try:
-        fetch_spacex_your_launch(folder_spacex, str(id))
+        fetch_spacex_your_launch(folder_spacex, str(id_spacex))
     except requests.exceptions.HTTPError as error:
         print("Can't get data from server:\n{0}.\nDownloading the last launch.".format(error))
         fetch_spacex_last_launch(folder_spacex)
